aae1f1f1471f6495e723d33d2b6433eb7988f084
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-05-31 13:07:25 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Theós on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
